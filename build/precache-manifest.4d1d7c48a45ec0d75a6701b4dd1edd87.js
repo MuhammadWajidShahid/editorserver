ea93fa821ebde7f923e1ccd39cc149902d9aa659
Muhammad Wajid <muhammad.wajid.shahid@gmail.com>
2019-05-21 12:21:08 +0500

    first commit

---
+++ build/precache-manifest.4d1d7c48a45ec0d75a6701b4dd1edd87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0f0a592e97a66437ac0c",
+    "url": "/static/js/main.08d65a21.chunk.js"
+  },
+  {
+    "revision": "0e0778bea0bc3e545857",
+    "url": "/static/js/2.09ad4f3c.chunk.js"
+  },
+  {
+    "revision": "0f0a592e97a66437ac0c",
+    "url": "/static/css/main.57cb81fd.chunk.css"
+  },
+  {
+    "revision": "0e0778bea0bc3e545857",
+    "url": "/static/css/2.64153366.chunk.css"
+  },
+  {
+    "revision": "5b2101945cbddbd8d3e96713afc948ef",
+    "url": "/index.html"
+  }
+];
---
+++ server.js
@@ -0,0 +1,135 @@
+const express = require("express");
+var spawn = require("child_process").spawn;
+let app = express();
+let server = require("http").createServer(app);
+let fs = require("fs");
+let bodyparser = require("body-parser");
+const io = require("socket.io")(server);
+const pty = require("node-pty");
+const rmdir= require('rimraf');
+var os = require('os');
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json())
+app.use(express.static("./build"));
+app.get("/", (req, res) => {
+    res.send("file");
+});
+io.on("connection", socket => {
+    console.log("connected" + socket.id);
+    let shell;
+    let exit = true;
+    // For all websocket data send it to the shell
+    socket.on('message', (msg) => {
+        shell.write(msg);
+        // console.log(msg)
+    });
+    socket.on("runcpp", (data) => {
+        console.log(data)
+        let done = true;
+        if (!fs.existsSync(`./programs/${socket.id}`)) {
+
+            fs.mkdirSync(`./programs/${socket.id}`, (err) => {
+                if (err)
+                    done = false;
+                else {
+                    done = true;
+                }
+            })
+        }
+        fs.writeFile(`./programs/${socket.id}/program.cpp`, data.code, (err) => {
+            if (err)
+                done = false;
+            else
+                done = true;
+        });
+        console.log(done)
+        if (done) {
+            if (!exit)
+                shell.kill();
+            var proc = spawn('gcc', [`./programs/${socket.id}` + '/' + `program` + ".cpp", "-o", `./programs/${socket.id}` + '/' + `program`, '-lstdc++']);
+            var stdout = "";
+            var stderr = "";
+            var f = false;
+            var timeout = setTimeout(function () {
+                proc.stdin.pause();
+                proc.kill();
+                f = true;
+            }, 2000);
+            proc.stderr.on('data', function (_stderr) {
+                stderr += _stderr;
+            });
+            proc.stderr.on('end', function () {
+                proc.kill();
+                f = true;
+            });
+            proc.on('close', function (code) {
+                proc.kill();
+                f = true;
+                if (stderr) {
+                    console.log(stderr)
+                }
+                else {
+                    console.log(f);
+                    if (f) {
+                        var name = os.platform() === 'win32' ? 'powershell.exe' : 'bash';
+                        shell = pty.spawn(name
+                            , ["./program.exe"]
+                            , {
+                                name: 'xterm-color',
+                                cwd: `${__dirname}/programs/${socket.id}`,
+                                env: process.env,
+                                shell: true
+                            });
+                            exit = false;
+                        socket.emit("running", true);
+                        // For all shell data send it to the websocket
+                        shell.on('data', (data) => {
+                            if(!exit)
+                            socket.emit("responce", data);
+                            // console.log(data)
+                        });
+                        // // For all websocket data send it to the shell
+                        // socket.on('message', (msg) => {
+                        //     shell.write(msg);
+                        //     console.log(msg)
+                        // });
+                        shell.on("exit", d => {
+                            exit = true;
+                            console.log("exited");
+                            socket.emit("responce", '\n');
+                            socket.emit("exit", true);
+                        });
+                    }
+                }
+
+            });
+            if (f) {
+                clearTimeout(timeout);
+            }
+        }
+
+
+    });
+
+    socket.on("disconnect",  function () {
+        if (!exit)
+         shell.kill();
+
+        rmdir(`./programs/${socket.id}`,(err)=>{
+            if(err)
+            console.log(err)
+        })
+        console.log("disconnect" + socket.id);
+    })
+});
+
+app.get("/",(req,res)=>{
+    res.send("jlo");
+})
+
+
+
+
+
+
+server.listen(800 || process.env.PORT, console.log("server running"))
